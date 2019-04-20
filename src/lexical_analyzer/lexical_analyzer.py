0f57846b2c37b5fd44a5e0967472dd6eb5cc9088
Khaled Hamed <khaled.hamedt@gmail.com>
2019-04-20 15:23:03 +0200

    formalize lexical analyzer

--- src/lexical_analyzer/lexical_analyzer.py
+++ src/lexical_analyzer/lexical_analyzer.py
@@ -1,26 +1,25 @@
 import re
 import csv
-from Token import Token
+from .Token import Token
 
 
-def main():
-    Token.load_dict()
+class LexicalAnalyzer():
+    def __init__(self):
+        Token.load_dict()
 
-    reg_list = [val for _, val in Token.token_dict.items()]
-    regex = '(' + '|'.join(reg_list) + ')'
+        reg_list = [val for _, val in Token.token_dict.items()]
+        self.regex = '(' + '|'.join(reg_list) + ')'
+        self.regex = re.compile(self.regex)
 
-    regex = re.compile(regex)
-    
-    code = open('sample_code.cpp').readlines()
-    code = ''.join(code)
-    result = regex.findall(code)
+    def analyze(self, code_file_name):
+        """Analyze code and return a stream of tokens."""
+        code = open(code_file_name).readlines()
+        code = ''.join(code)
+        result = self.regex.findall(code)
 
-    tokens = [Token(i) for i in result]
-    
-    with open('output.txt', 'w') as out:
-        for token in tokens:
-            out.write(str(token) + '\n')
+        tokens = [Token(i) for i in result]
 
-
-if __name__ == "__main__":
-    main()
+        with open('tokens.txt', 'w') as out:
+            for token in tokens:
+                out.write(str(token) + '\n')
+        return tokens
