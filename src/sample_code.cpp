81036e8dfafac1e2ea3e7b699896c5c6ead74d7f
Mostafa Saber <mostafa.saber3789@gmail.com>
2019-04-28 16:27:13 +0200

    more tests

--- src/sample_code.cpp
+++ src/sample_code.cpp
@@ -7,10 +7,14 @@ bool isPowerOfTwo(int x) {
 	float f = 0.65; // extra added to rules
 	int x = 5;
 	int y = 6;
-	int z = x - y;
-//	z = x + y;
+	int z = x;
     // string hi = "hiasjfn;lwqmef;qwefqewf"; NOT IN GRAMMAR RULES SENT
     char k = 'k';
     bool f = true;
+    int arr[];
+
+    arr = new int[10];
+    z = x + y;
+
 	return x && (!(x & (x - 1)));
 }
