d4cf00a7b2b9307740c90c2f8f0f21fe24ab4c1a
Khaled Hamed <khaled.hamedt@gmail.com>
2019-05-02 00:25:24 +0200

    update grammar

--- src/sample_code.cpp
+++ src/sample_code.cpp
@@ -1,17 +1,24 @@
-bool isPowerOfTwo(int x) {
+bool isPowerOfTwo(int x, int y[]) {
     /*
     Try multiline comment
     */
 	// First x in the below expression is
 	// for the case when x is 0
-	if (x == 0) 
-		if (x == 1)
-			return true;
-		else
-			return false;
-		// else
-		// 	return false;
+	
+	int x, y;
+	while (x > 10) {
+		if (x == 0) 
+			if (x == 1)
+				return true;
+			else
+				return false;
+			// else
+			// 	return false;
+		x = x + 1;
+		break;
+	}
 		
 
 	return x && (!(x & (x - 1)));
 }
+
